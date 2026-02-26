0cdbf28e57852a582c1aabe3aa4b71f41860064b
Dimitri Simonyan <simadimonyan@gmail.com>
2026-02-26 17:50:30 +0300

    + Task 3 (stream solution)

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -1,17 +1,23 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
 /*
 Задача 3
-Отсортировать коллекцию сначала по фамилии, по имени (при равной фамилии), и по дате создания (при равных фамилии и имени)
+Отсортировать коллекцию сначала по фамилии, по имени (при равной фамилии),
+и по дате создания (при равных фамилии и имени)
  */
 public class Task3 {
 
   public static List<Person> sort(Collection<Person> persons) {
-    return new ArrayList<>(persons);
+    return persons.stream()
+            .sorted(Comparator.comparing(Person::secondName)
+                .thenComparing(Person::firstName)
+                .thenComparing(Person::createdAt)
+            ).toList();
   }
+
 }
