1687d7c382dd6dfd56734aa5585ae00ac5e98aad
Dimitri Simonyan <simadimonyan@gmail.com>
2026-02-26 22:34:18 +0300

    + Task 6 (stream solution)

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,10 +2,12 @@ package tasks;
 
 import common.Area;
 import common.Person;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -19,6 +21,17 @@ public class Task6 {
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+
+    Map<Integer, Area> areaMap = areas.stream()
+            .collect(Collectors.toMap(Area::getId, Function.identity()));
+
+    return persons.stream()
+            .flatMap(person -> personAreaIds.get(person.id()).stream()
+                .map(id -> {
+                    Area area = areaMap.get(id);
+                    return person.firstName() + " - " + area.getName();
+                }
+            )).collect(Collectors.toSet());
   }
+
 }
