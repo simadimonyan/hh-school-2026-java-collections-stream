7b3dc102429336cf038523bb2c6ae9af14f26b20
Dimitri Simonyan <simadimonyan@gmail.com>
2026-03-02 22:59:37 +0300

    + Task 9 (refactor solution)

--- src/main/java/tasks/Task9.java
+++ src/main/java/tasks/Task9.java
@@ -20,12 +20,8 @@ public class Task9 {
   // Костыль, эластик всегда выдает в топе "фальшивую персону".
   // Конвертируем начиная со второй
   public List<String> getNames(List<Person> persons) {
-//    + Проверка не нужна, список и так будет пустым
-//    if (persons.size() == 0) {
-//      return Collections.emptyList();
-//    }
-//    + Добавлен скип для читаемости
-//    persons.remove(0);
+    // + Добавлен скип для читаемости
+    // + skip пропускает по условию, а remove удаляет
     return persons.stream().skip(1).map(Person::firstName).collect(Collectors.toList());
   }
 
@@ -37,20 +33,7 @@ public class Task9 {
 
   // Тут фронтовая логика, делаем за них работу - склеиваем ФИО
   public String convertPersonToString(Person person) {
-//    String result = "";
-//    if (person.secondName() != null) {
-//      result += person.secondName();
-//    }
-//
-//    if (person.firstName() != null) {
-//      result += " " + person.firstName();
-//    }
-//
-//    if (person.secondName() != null) {
-//      result += " " + person.secondName();
-//    }
-
-//  + Добавлен stream для читаемости (сверху баг, пропущено middle name)
+    // + Добавлен stream для читаемости (сверху баг, пропущено middle name)
     return Stream.of(person.firstName(), person.middleName(), person.secondName())
             .filter(Objects::nonNull)
             .collect(Collectors.joining(" "));
@@ -58,38 +41,24 @@ public class Task9 {
 
   // словарь id персоны -> ее имя
   public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-//    Map<Integer, String> map = new HashMap<>(1);
-//    for (Person person : persons) {
-//      if (!map.containsKey(person.id())) {
-//        map.put(person.id(), convertPersonToString(person));
-//      }
-//    }
-
-//  + Добавлен stream для читаемости
-    return persons.stream().collect(Collectors.toMap(Person::id, Person::firstName));
+    // + Добавлен stream для читаемости
+    return persons.stream()
+            .collect(Collectors.toMap(
+                    Person::id,
+                    this::convertPersonToString,
+                    (existing, replacement) -> existing
+            ));
   }
 
   // есть ли совпадающие в двух коллекциях персоны?
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-//    boolean has = false;
-//    for (Person person1 : persons1) {
-//      for (Person person2 : persons2) {
-//        if (person1.equals(person2)) {
-//          has = true;
-//        }
-//      }
-//    }
-
-//  + Добавлен stream для читаемости
+    // + Добавлен stream для читаемости
     return persons1.stream().anyMatch(persons2::contains);
   }
 
   // Посчитать число четных чисел
   public long countEven(Stream<Integer> numbers) {
-//    count = 0;
-//    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-
-//  + Использован встроенный метод count() у stream для читаемости
+    // + Использован встроенный метод count() у stream для читаемости
     return numbers.filter(num -> num % 2 == 0).count();
   }
 
@@ -100,7 +69,12 @@ public class Task9 {
     List<Integer> snapshot = new ArrayList<>(integers);
     Collections.shuffle(integers);
     Set<Integer> set = new HashSet<>(integers);
-    // + совпадение (хешкода чисел % количество элементов в хешсете) с бакетами хешсета по порядку :D
+    // + Хеш сет хранит элементы в бакетах на основе хеш-кода.
+    // + У integer хеш-код равен самому числу, поэтому числа 1..10000 распределяются по бакетам равномерно.
+    // + При обходе хеш сет проходит бакеты по порядку так как его ключи будут вычисляться так
+    // + -> (хешкода чисел % количество элементов в хешсете), поэтому порядок случайно совпадает с исходным списком.
+    // + Из за этого сет сортируется и assert == true.
+    // + Но если поменять размер сета то все поломается
     assert snapshot.toString().equals(set.toString());
   }
 }
